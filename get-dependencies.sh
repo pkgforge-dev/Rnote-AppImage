ae121b8eb2ee9fbc769548d724b1907a91e6148b
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-18 01:38:09 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -42,10 +42,7 @@ echo "---------------------------------------------------------------"
 sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
 git clone https://gitlab.archlinux.org/archlinux/packaging/packages/rnote.git ./rnote && (
 	cd ./rnote
-	sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
-        pacman -Syu --noconfirm gtk4 glib2 libadwaita poppler-glib gstreamer alsa-lib \
-	                        meson cargo cmake clang git
-	makepkg -f
+    makepkg -fs --noconfirm
 	ls -la .
 	pacman --noconfirm -U *.pkg.tar.*
 )
