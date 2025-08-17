44128ec2a3b7465f1d7004a5f0b12a34c1490eb1
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-18 01:47:06 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -42,6 +42,7 @@ echo "---------------------------------------------------------------"
 sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
 git clone https://gitlab.archlinux.org/archlinux/packaging/packages/rnote.git ./rnote && (
 	cd ./rnote
+	sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
     makepkg -fs --noconfirm
 	ls -la .
 	pacman --noconfirm -U *.pkg.tar.*
