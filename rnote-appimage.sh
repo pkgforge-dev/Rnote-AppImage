6bcabf85740e1835fbee96a09be21ea9f4fd2c1f
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-17 20:08:34 +0200

    Revert to original state

--- rnote-appimage.sh
+++ rnote-appimage.sh
@@ -15,6 +15,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export OUTNAME="$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
 export DESKTOP=/usr/share/applications/com.github.flxzt.rnote.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/com.github.flxzt.rnote.svg
+export PATH_MAPPING_RELATIVE=1 # GTK applications are usually hardcoded to look into /usr/share, especially noticeable in non-working locale, looking for better solution which doesn't change working directory
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 export DEPLOY_LOCALE=1
@@ -39,9 +40,6 @@ StartupWMClass=rnote
 
 ## Further debloat locale
 find ./AppDir/share/locale -type f ! -name '*glib*' ! -name '*rnote*' -delete
-
-## Make locale directory recognizable in AppImage directory
-echo 'TEXTDOMAINDIR=${SHARUN_DIR}/share/locale' >> ./AppDir/.env
 
 # MAKE APPIMAGE WITH URUNTIME
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
