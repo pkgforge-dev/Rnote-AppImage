a5fb9afc762afb9f69c1fdf095c6edcfa667cfae
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-22 15:04:01 +0200

    `get-dependencies`: Update it to reflect upstream URLs & simplify it

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,15 +7,16 @@ sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
 ARCH="$(uname -m)"
 
 if [ "$ARCH" = 'x86_64' ]; then
-	PKG_TYPE='x86_64.pkg.tar.zst'
-else
-	PKG_TYPE='aarch64.pkg.tar.xz'
+	PKG_TYPE="$ARCH.pkg.tar.zst"
+elif [ "$ARCH" = 'aarch64' ]; then
+	PKG_TYPE="$ARCH.pkg.tar.xz"
 fi
 
-LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
-OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
-MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-nano-$PKG_TYPE"
-INTEL_MEDIA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/intel-media-mini-$PKG_TYPE" 
+LIBXML2_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/libxml2-mini-$PKG_TYPE"
+OPUS_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/opus-mini-$PKG_TYPE"
+MESA_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/mesa-nano-$PKG_TYPE"
+INTEL_MEDIA_DRIVER_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/intel-media-driver-mini-$PKG_TYPE" 
+GTK4_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/gtk4-mini-$PKG_TYPE"
 
 echo "Installing build dependencies for sharun & AppImage integration..."
 echo "---------------------------------------------------------------"
@@ -42,16 +43,16 @@ git clone https://gitlab.archlinux.org/archlinux/packaging/packages/rnote.git ./
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$LIBXML_URL"  -O  ./libxml2-iculess.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$OPUS_URL"    -O  ./opus-nano.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$MESA_URL"        -O  ./mesa.pkg.tar.zst
-
+wget --retry-connrefused --tries=30 "$LIBXML2_URL"  -O  ./"libxml2-mini-$PKG_TYPE"
+wget --retry-connrefused --tries=30 "$OPUS_URL"    -O  ./"opus-mini-$PKG_TYPE"
+wget --retry-connrefused --tries=30 "$MESA_URL"        -O  ./"mesa-nano-$PKG_TYPE"
+wget --retry-connrefused --tries=30 "$GTK4_URL"        -O  ./"gtk4-mini-$PKG_TYPE"
 if [ "$ARCH" = 'x86_64' ]; then
-	wget --retry-connrefused --tries=30 "$INTEL_MEDIA_URL"  -O ./intel-media.pkg.tar.zst
+  wget --retry-connrefused --tries=30 "$INTEL_MEDIA_DRIVER_URL"  -O ./"intel-media-driver-mini-$PKG_TYPE"
 fi
 
-pacman -U --noconfirm ./*.pkg.tar.zst
-rm -f ./*.pkg.tar.zst
+pacman -U --noconfirm ./*.pkg.*
+rm -f ./*.pkg.*
 
 echo "All done!"
 echo "---------------------------------------------------------------"
