4651c401dd6a8b24eeecf561722b97c7364c8cd6
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-08 10:09:20 +0100

    Simplify the AppImage making script

--- rnote-appimage.sh
+++ rnote-appimage.sh
@@ -3,39 +3,29 @@
 set -eux
 
 ARCH="$(uname -m)"
-PACKAGE=rnote
-URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 VERSION="$(cat ~/version)"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
 # Variables used by quick-sharun
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export OUTNAME="$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
+export OUTNAME=rnote-"$VERSION"-anylinux-"$ARCH".AppImage
 export DESKTOP=/usr/share/applications/com.github.flxzt.rnote.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/com.github.flxzt.rnote.svg
 export DEPLOY_OPENGL=1
 export STARTUPWMCLASS=rnote # For Wayland, this is 'com.github.flxzt.rnote', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
-# DEPLOY ALL LIBS
+# Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
-./quick-sharun /usr/bin/rnote /usr/bin/rnote-cli
-
-## Copy fonts used for rnote
-cp -vr /usr/share/fonts/rnote-fonts ./AppDir/share/fonts/
-
-## Further debloat locale
-find ./AppDir/share/locale -type f ! -name '*glib*' ! -name '*rnote*' -delete
+./quick-sharun /usr/bin/rnote /usr/bin/rnote-cli /usr/share/fonts/rnote-fonts
 
 ## Set gsettings to save to keyfile, instead to dconf
 echo "GSETTINGS_BACKEND=keyfile" >> ./AppDir/.env
 
-# MAKE APPIMAGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
-chmod +x ./uruntime2appimage
-./uruntime2appimage
+# Make the AppImage with uruntime
+./quick-sharun --make-appimage
 
-# PREPARE APPIMAGE FOR RELEASE
+# Prepare the AppImage for release
 mkdir -p ./dist
 mv -v ./*.AppImage* ./dist
 mv -v ~/version     ./dist
