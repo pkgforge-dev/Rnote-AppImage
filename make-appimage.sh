fd4f64d32182fdd974deeb513ad53881e0210e6e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-17 07:20:20 +0100

    Add `self-updater` back
    Now it will skip if AppImage managers are installed and has 5 seconds delay instead of 30, so it's good now.

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,6 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q rnote | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/com.github.flxzt.rnote.svg
 export DESKTOP=/usr/share/applications/com.github.flxzt.rnote.desktop
