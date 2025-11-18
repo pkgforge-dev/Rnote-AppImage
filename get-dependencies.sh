e3c73676292c63e147eda44d8aa335d06a41236d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-18 18:53:14 +0100

    Simplify building `rnote`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,7 @@ ARCH=$(uname -m)
 
 echo "Building package and its dependencies..."
 echo "---------------------------------------------------------------"
-sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
-git clone https://gitlab.archlinux.org/archlinux/packaging/packages/rnote.git ./rnote && (
-	cd ./rnote
-	sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
-        makepkg -fs --noconfirm
-	ls -la .
-	pacman --noconfirm -U *.pkg.tar.*
-)
+make-aur-package --archlinux-pkg rnote
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
