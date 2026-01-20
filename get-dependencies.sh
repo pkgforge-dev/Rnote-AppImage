9418b27fe6c8d3f03261ea36b17438962c33d731
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-20 19:44:07 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,10 +4,10 @@ set -eu
 
 ARCH=$(uname -m)
 
-echo "Building package and its dependencies..."
-echo "---------------------------------------------------------------"
-make-aur-package --archlinux-pkg rnote
-
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
+
+echo "Building package and its dependencies..."
+echo "---------------------------------------------------------------"
+make-aur-package --archlinux-pkg rnote
