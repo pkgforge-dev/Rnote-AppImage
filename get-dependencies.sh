e28a63e7df02667365c7919c8acd12e9bb0a6a39
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-23 15:18:08 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,8 @@ ARCH=$(uname -m)
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+# Cannot build Rust appa with llvm-libs-mini
+get-debloated-pkgs --add-common --prefer-nano ! llvm-libs
 
 echo "Building package and its dependencies..."
 echo "---------------------------------------------------------------"
